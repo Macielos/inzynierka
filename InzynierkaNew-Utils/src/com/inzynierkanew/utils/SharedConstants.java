ef115a4f048c9775a3ed22a41fb7f9f6f79b3bbe
Macielos <maciek.ganko@gmail.com>
2015-12-09 20:26:03 +0100

    Dodanie projektu Utils

---
+++ InzynierkaNew-Utils/src/com/inzynierkanew/utils/SharedConstants.java
@@ -0,0 +1,37 @@
+package com.inzynierkanew.utils;
+
+public class SharedConstants {
+
+	//TODO przemyslec co musi byc shared a co moze byc tylko po jednej ze stron
+	public static final int MAX_HERO_LEVEL = 100;
+	public static final int BASE_XP_PER_LEVEL = 1000;
+	public static final int NEXT_LEVEL_FACTOR = 500;
+	
+	public static final int BASE_GOLD_PER_VICTORY = 300;
+	public static final double GOLD_XP_MODIFIER = 0.1;
+	public static final double GOLD_RANDOM_FACTOR = 0.5;
+	
+	public static final double CHANCE_FOR_ITEM = 1.0; //0.6
+	public static final int LEVELS_PER_NEXT_ITEM_LEVEL = 5;
+	public static final int LEVELS_PER_NEXT_ITEM_SLOT = 5;
+	public static final int MAX_EQUIPPED_ITEMS = 6;
+	public static final int INVENTORY_SIZE = 30;
+	
+	public static final int INITIAL_SKILL_VALUE = 10;
+	
+	public static final int INITAL_SKILL_POINTS = 25;
+	
+	public static final int HERO_ITEM_LEVEL_DIFF = 5;
+	
+	public static final String STANDARD = "STANDARD";
+	public static final String MAGICAL = "MAGICAL";
+	public static final String LEGENDARY = "LEGENDARY";
+	
+	public static final double STANDARD_ITEM_STOCK_FACTOR = 1.0;
+	public static final double MAGICAL_ITEM_STOCK_FACTOR = 0.4;
+	public static final double LEGENDARY_ITEM_STOCK_FACTOR = 0.1;
+	
+	public static final String SEPARATOR = ",";
+	
+	
+}
